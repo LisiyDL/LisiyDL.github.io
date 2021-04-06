e38cd8a49d8ec582789430f104c637e1a1bae50a
LisiyDL <ematiushkov@gmail.com>
2021-04-07 00:40:51 +0300

    )

---
+++ loudbook/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+	$('.hits_right').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		prevArrow: '<button type="button" class="leftH"></button>',
+    	nextArrow: '<button type="button" class="rightH"></button>',
+	});
+})
+$(function(){
+	$('.NItems_right').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		prevArrow: '<button type="button" class="leftH"></button>',
+    	nextArrow: '<button type="button" class="rightH"></button>',
+	});
+})
+$(function(){
+	$('.tst_main_block').slick({
+	  infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  prevArrow: '<button type="button" class="leftT"></button>',
+	  nextArrow: '<button type="button" class="rightT"></button>',
+});
+})
